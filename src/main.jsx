4ff379a34afa46c9c12e2a6eee35ff6df563db94
unknown <kaleb2azriel@gmail.com>
2017-08-18 00:20:32 -0700

    Including fonts in webpack

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {render} from 'react-dom';
 require("./index.html");
+require('./fonts/nowayregular.scss');
 require('./sass/Blog.scss');
 class Blog extends React.Component {
   render () {
--- webpack.config.js
+++ webpack.config.js
@@ -19,10 +19,37 @@ var config = {
         test: /\.html$/,
         loader: "file-loader?name=[name].[ext]",
       },
+      { 
+        test: /\.css$/, 
+        loader: "style-loader!css-loader" 
+      },
       {
         test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader']
-      }
+        exclude: /node_modules/,
+        loaders: ['style-loader', 'css-loader', 'resolve-url-loader', 'sass-loader']
+      },
+      {
+        test: /\.(jpe?g|png)$/,
+        loader: "file-loader?name=[path][name].[ext]"
+     },
+     {
+       test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+       exclude: /node_modules/,
+       loader: 'url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]'
+     },
+     {
+       test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+       exclude: /node_modules/,
+       loader: 'url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
+     },
+     {
+       test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+       loader: 'file-loader?name=fonts/[name].[ext]'
+     },
+     {
+       test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+       loader: 'url-loader?limit=10000&mimetype=image/svg+xml'
+     }
     ]
   },
   devServer: {

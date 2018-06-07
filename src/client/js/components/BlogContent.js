13f6f98c2bbb7e7c3fd5764838c75e8b1415c0fb
andrew sunada <kaleb2azriel@gmail.com>
2018-06-06 20:23:35 -0700

    Adding blog list with titles/dates

--- src/client/js/components/BlogContent.js
+++ src/client/js/components/BlogContent.js
@@ -4,6 +4,17 @@ import PropTypes from 'prop-types';
 require('../../sass/components/BlogContent.scss');
 export default class BlogContent extends React.Component {
     render () {
-        return (<section className="blog-content"><p>Welcome to my blog!</p></section>);
+        return (<section className="blog-content">
+            <ul className="blog-list">
+                <li className="blog-list-item">
+                    <label className="list-item-title">Blog Title</label>
+                    <time className="list-item-date">01/01/2018</time>
+                </li>
+                <li className="blog-list-item">
+                    <label className="list-item-title">Blog Title</label>
+                    <time className="list-item-date">01/01/2018</time>
+                </li>
+            </ul>
+        </section>);
     }
 }
